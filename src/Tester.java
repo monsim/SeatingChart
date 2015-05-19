cf63cd266ce6ed0da4b6a886e80c08f7154f03a9
unknown <mmagal203@FHS-LAB127-LEN3.fhs.win.fuhsd.org>
2015-05-19 08:19:57 -0700

    third commit

--- src/Tester.java
+++ src/Tester.java
@@ -30,29 +30,53 @@ public class Tester {
 		int firstStudentIndex = (int)Math.random()*(students.size());
 		Student firstStudent = students.get(firstStudentIndex);
 		Student[][] classroom = new Student[rows][cols];
-		int counter = 0;
+		int counter = 0; //what's the point of counter again
 		classroom[0][0] = firstStudent; 
 		Student previousStudent = firstStudent;
 		while (counter < students.size() - 1) {
 			for (int r = 0; r < rows; r++) {
-				for (int c = 0; c < cols; c++) {
+				for (int c = 1; c < cols; c++) {	//[0][0] has already been set
 					System.out.println("cool");
-					Student nextStudent = findNextStudent(previousStudent, students);
+					
 					if (r != 0) {
-						//someone above, need to double check they can sit next to them too
+						//someone above 
 						//INCREMENT COUNTER SOMEWHERE
+						previousStudent = classroom[r-1][c];
+						Student nextStudent = findNextStudent(previousStudent, students);
+						if (c != 0) { //someone to the left as well
+							Student left = classroom[r][c-1];
+							while (!InArrayList(left, nextStudent, students)) {
+								nextStudent = findNextStudent(previousStudent, students);
+							}
+							classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's and left's arrayList
+						}
+					}		
+					
+					if (r == 0) {
+						//below hasn't been initialized, it doesn't matter. only one that matters is to the left
+						previousStudent = classroom[r][c-1];
+						Student nextStudent = findNextStudent(previousStudent, students);
+						classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's arrayList
 					}
-					classroom[r][c] = nextStudent;
-					previousStudent = nextStudent;
 					counter++;
 				}
 			}
 		}
 	}
 
-	private static Student findNextStudent(Student previousStudent, ArrayList<Student> students) {
+	private static boolean InArrayList(Student left, Student nextStudent, ArrayList<Student> students) {
+		ArrayList<String> otherStudents = left.getPossibleStudents();
+		for (Student s : students) {
+			if (s.getName().equals(nextStudent)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static Student findNextStudent(Student previousStudent, ArrayList<Student> students) {		//randomly generates student
 		ArrayList<String> otherStudents = previousStudent.getPossibleStudents();
-		String seatedNext = otherStudents.get((int)(Math.random()*otherStudents.size()));
+		String seatedNext = otherStudents.get((int)(Math.random()*otherStudents.size())); 
 		Student next = null;
 		for (Student s : students) {
 			if (s.getName().equals(seatedNext)) {
