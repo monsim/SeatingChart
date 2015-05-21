f8a7ef40f425d83b43933b4c22915fccc3fe958e
Monsi <m_mswim@yahoo.com>
2015-05-21 08:19:57 -0700

    fourth commit

--- src/Tester.java
+++ src/Tester.java
@@ -10,8 +10,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		ArrayList<String> studentList = new ArrayList<String>();
-		String fileName = JOptionPane
-				.showInputDialog("What is the file name of the students?");
+		String fileName = JOptionPane.showInputDialog("What is the file name of the students?");
 
 		loadDataFromFile(fileName, studentList);
 		
@@ -26,7 +25,10 @@ public class Tester {
 		
 		for (int r = 0; r < classroom[0].length; r++) {
 			for (int c = 0; c < classroom.length; c++) {
-				System.out.print(classroom[r][c].getName() + "     ");
+				Student student = classroom[r][c];
+				//System.out.print(student.getName() + "    ");
+				//System.out.println(classroom.length);
+				//System.out.println(classroom[0].length);
 			}
 			System.out.println();
 		}
@@ -37,6 +39,27 @@ public class Tester {
 		if (again.equals("Y") && same.equals("Y")) {
 			generateChartAgain(rows,cols,students, classroom);
 		}
+		
+		if (again.equals("Y") && same.equals("N")) {  //think of a better way to do this
+			String fileName1 = JOptionPane.showInputDialog("What is the file name of the students?");
+			loadDataFromFile(fileName, studentList);
+			
+			ArrayList<Student> students1 = convertToStudents(studentList);
+			
+			String numRows1 = JOptionPane.showInputDialog("How many rows do you want?");
+			String numCols1 = JOptionPane.showInputDialog("How many columns do you want?");
+			int rows1 = Integer.parseInt(numRows);
+			int cols1 = Integer.parseInt(numCols);
+			
+			Student[][] classroom1 = generateChart(rows1, cols1, students1);
+			
+			for (int r = 0; r < classroom[0].length; r++) {
+				for (int c = 0; c < classroom.length; c++) {
+					System.out.print(classroom[r][c].getName() + "\t");
+				}
+				System.out.println();
+			}
+		}
 
 	}
 
@@ -45,35 +68,42 @@ public class Tester {
 	}
 
 	private static Student[][] generateChart(int rows, int cols, ArrayList<Student> students) {
-		int firstStudentIndex = (int)Math.random()*(students.size());
-		Student firstStudent = students.get(firstStudentIndex);
+		int firstStudentIndex = 0;
+		Student firstStudent = null;
 		Student[][] classroom = new Student[rows][cols];
 		int counter = 0; //what's the point of counter again
-		classroom[0][0] = firstStudent; 
-		Student previousStudent = firstStudent;
+		Student previousStudent = null;
 		while (counter < students.size() - 1) {
 			for (int r = 0; r < rows; r++) {
-				for (int c = 1; c < cols; c++) {	//[0][0] has already been set
-					System.out.println("cool");
-					
-					if (r != 0) {
-						//someone above 
-						previousStudent = classroom[r-1][c];
-						Student nextStudent = findNextStudent(previousStudent, students);
-						if (c != 0) { //someone to the left as well
-							Student left = classroom[r][c-1];
-							while (!inArrayList(left, nextStudent, students)) {
-								nextStudent = findNextStudent(previousStudent, students);
+				for (int c = 0; c < cols; c++) {	//[0][0] has already been set					
+					if (r == 0 && c == 0) {
+						firstStudentIndex = (int)Math.random()*(students.size());
+						firstStudent = students.get(firstStudentIndex);
+						classroom[0][0] = firstStudent; 
+					} else {
+						if (r != 0) {
+							//someone above 
+							previousStudent = classroom[r-1][c];
+							//System.out.println(previousStudent.getName());
+							Student nextStudent = findNextStudent(previousStudent, students);
+							//System.out.println(nextStudent.getName());
+							if (c != 0) { //someone to the left as well
+								Student left = classroom[r][c-1];
+								//System.out.println(left.getName());
+								while (!inArrayList(left, nextStudent, students)) {
+									nextStudent = findNextStudent(previousStudent, students);
+								}
+								classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's and left's arrayList
 							}
-							classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's and left's arrayList
-						}
-					}		
+						}		
 					
-					if (r == 0) {
-						//below hasn't been initialized, it doesn't matter. only one that matters is to the left
-						previousStudent = classroom[r][c-1];
-						Student nextStudent = findNextStudent(previousStudent, students);
-						classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's arrayList
+						if (r == 0) {
+							//below hasn't been initialized, it doesn't matter. only one that matters is to the left
+							previousStudent = classroom[r][c-1];
+							System.out.println(previousStudent.getName());
+							Student nextStudent = findNextStudent(previousStudent, students);
+							classroom[r][c] = nextStudent; //remove nextStudent from previousStudent's arrayList
+						}
 					}
 					counter++;
 				}
